580c1105513ef21d659178d5ba3f0ad661b95c09
Unknown <gregoryivanoff6@gmail.com>
2018-08-30 23:39:36 +0300

    BMI methods added

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
+from index_calc import *
 
 app = Flask(__name__)
 
@@ -13,10 +14,17 @@ class Data(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     email_=db.Column(db.String(120), unique=True)
     height_=db.Column(db.Integer)
+    weight_=db.Column(db.Integer)
+    bmi_=db.Column(db.Numeric)
+    broc_ =db.Column(db.Numeric)
 
-    def __init__(self,email_,height_):
+    def __init__(self,email_,height_,weight_,bmi_,broc_):
         self.email_=email_
         self.height_=height_
+        self.weight_=weight_
+        self.bmi_=bmi_
+        self.broc_=broc_
+
 
 @app.route('/')
 def hello_world():
@@ -27,15 +35,18 @@ def success():
     if request.method=='POST':
         email=request.form["email_name"]
         height=request.form["height_name"]
-        print(email, height)
+        weight=request.form["weight_name"]
+        bmi = calc_bmi(height, weight)
+        broc = calc_broc(height)
+        print(email, height, weight, bmi, broc)
         if db.session.query(Data).filter(Data.email_==email).count() == 0:
-            data=Data(email,height)
+            data=Data(email,height,weight,bmi,broc)
             db.session.add(data)
-            #send_email(email,height)
+           # send_email(email,height)
             db.session.commit()
             return render_template("success.html")
-        return render_template("index.html",
-                                   text="Doubled email. Try another.")
+
+        return render_template("index.html",text="Doubled email. Try another.")
 
 if __name__ == '__main__':
     app.debug=True
--- index_calc.py
+++ index_calc.py
@@ -10,6 +10,15 @@ def met_calc(height):
 # Calculation of Body Mass Index
 # WARN: Banker's rounding
 def calc_bmi(height,weight):
-    bmi=round(weight/height**2,2)
-    return bmi;
+    try:
+        bmi=round(int(weight)/(int(height)/100)**2,2)
+        return bmi
+    except ZeroDivisionError:
+        return 0
 
+def calc_broc(height):
+    if (int(height)>150) and (int(height)<170):
+        broc=(int(height)-100)+0.1
+        return broc
+    else:
+        return 0
