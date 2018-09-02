b938fac92ee68f842cdbe4d64fdeb8df817c92af
Unknown <gregoryivanoff6@gmail.com>
2018-09-02 23:40:22 +0300

    Method for different BMIs

--- index_calc.py
+++ index_calc.py
@@ -15,10 +15,30 @@ def calc_bmi(height,weight):
         return bmi
     except ZeroDivisionError:
         return 0
-
+# Broc index
+# Normal weight depends on height
 def calc_broc(height):
     if (int(height)>150) and (int(height)<170):
         broc=(int(height)-100)+0.1
         return broc
     else:
         return 0
+# Breight index
+# Normal weight depends on height
+def calc_breit(height):
+    weight=height*0.7-50
+    return weight
+#Deventrope index
+def calc_dave(height,weight):
+    dave=round(int(weight)*1000/(int(height))**2,2)
+    return dave
+#Noord index
+# Normal weight depends on height
+def calc_noord(height,weight):
+    noord=(int(height)*420)/1000
+    return noord
+#Tatoon index
+# Normal weight depends on height
+def calc_tatoon(height,weight):
+    tatoon=int(height)-(100+(int(height)-100/20))
+    return tatoon
